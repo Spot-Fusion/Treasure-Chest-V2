94a4c01a106f437f91084b5fc8870a66b7e1e435
Thomas Garza <tbgarza2@gmail.com>
2020-04-09 19:24:23 -0500

    added login screen and made button go to home screen

--- src/App.js
+++ src/App.js
@@ -3,36 +3,31 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route, 
+  Redirect
 } from "react-router-dom";
 import HomePage from './pages/HomePage'
 import UsersPage from './pages/UsersPage'
 import MessagesPage from './pages/MessagesPage'
 import LoginPage from './pages/LoginPage';
+import DrawerNav from './components/DrawerNav'
 
 function App() {
+  const [menuVisible, setMenuVisible] = React.useState(false) 
+  const [redirect, setRedirect] = React.useState(false)
+
+  const setMenu = () => {
+    setMenuVisible(!menuVisible);
+    setRedirect(!redirect);
+  };
+
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-          <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/messages">Messages</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+       {menuVisible ? <DrawerNav /> : null}
+       {redirect ? <Redirect to={{pathname: '/home'}} /> : null }
         <Switch>
-        <Route exact path="/" render={() => (<LoginPage />)} />
+          <Route exact path="/" render={() => (<LoginPage setMenu={setMenu}/>)} />
           <Route exact path="/messages" render={() => (<MessagesPage />)} />
           <Route exact path="/users" render={() => (<UsersPage />)} />          
           <Route exact path="/home" render={() => (<HomePage />)} />            
