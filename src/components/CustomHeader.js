c025922af0ee79ce6d415a4d9e8236e638952aba
Thomas Garza <tbgarza2@gmail.com>
2020-04-10 16:38:42 -0500

    created header nav bar with menu button

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import { IoIosMenu } from "react-icons/io";
+import { Navbar } from 'react-bootstrap';
 
-function CustomHeader() {
+function CustomHeader({slideMenu}) {
+  
     return (
-        <div>
-            
-        </div>
+        <header>
+            <Navbar style={{backgroundColor: '#223843', width: '100%', height: '30px', top: '30px' }}>               
+                <IoIosMenu style={{left: '30px', color: '#F1F3F5'}} size={40} onClick={slideMenu} />   
+                <Navbar.Brand style={{alignSelf: 'center', color: '#F1F3F5'}} >Header</Navbar.Brand>            
+            </Navbar>
+        </header>
     )
 }
 
