f9f8f51bf24ca4df42f1f45647a0ea9c436a7860
Thomas Garza <tbgarza2@gmail.com>
2020-04-10 16:40:26 -0500

    created nav drawer still needs css work and "sliding capabilities"

--- src/components/DrawerNav.js
+++ src/components/DrawerNav.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import * as React from 'react'
 import { Link } from 'react-router-dom'
+import { 
+    IoMdContact,
+    IoMdExit,
+    IoMdHome,
+} from "react-icons/io";
+import { MdMessage } from 'react-icons/md'
+// import { slide as Menu } from 'react-burger-menu'
+import { Navbar } from 'react-bootstrap';
 
-function DrawerNav() {
+function DrawerNav({ slideMenu }) {
     return (
-        <div>
-        <nav>
-          <ul>
-          <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
+        <div >
+        <Navbar styles={styles}>
+          <ul>            
+            <li onClick={slideMenu}>
+              <IoMdHome color={'#223843'} size={20} />
               <Link to="/home">Home</Link>
             </li>
-            <li>
+            <li onClick={slideMenu}>
+              <MdMessage color={'#223843'} size={20} />
               <Link to="/messages">Messages</Link>
             </li>
-            <li>
+            <li onClick={slideMenu}>
+              <IoMdContact color={'#223843'} size={20} />
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li onClick={slideMenu}>
+              <IoMdContact color={'#223843'} size={20} />
               <Link to="/users">Users</Link>
             </li>
+            <li onClick={slideMenu}>
+              <IoMdExit color={'#223843'} size={20} />
+              <Link to="/">LogOut</Link>
+            </li>
           </ul>
-        </nav>
+        </Navbar>
         </div>
     )
 }
 
 export default DrawerNav
+
+var styles = {  
+    position: 'fixed',
+    zIndex: '200',
+    top: '56px',
+    left: '0',
+    backgroundColor: '#F1F3F5',
+    padding: '2.5em 1.5em 0',
+    height: '100%',
+    width: '75%', 
+    opacity: '100%'
+ 
+}
