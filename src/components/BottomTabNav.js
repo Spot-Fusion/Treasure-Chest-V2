a4fba05575e809b2e9205f8621eb7a626c2e5e29
Thomas Garza <tbgarza2@gmail.com>
2020-04-10 16:37:46 -0500

    functioning bottom tab nav

--- src/components/BottomTabNav.js
+++ src/components/BottomTabNav.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import * as React from 'react'
+import { Link } from 'react-router-dom'
+import { 
+    IoMdAddCircleOutline,
+    IoMdAddCircle,
+    IoMdHome,
+} from "react-icons/io";
+import { MdMessage } from 'react-icons/md'
+import { Navbar } from 'react-bootstrap';
 
 function BottomTabNav() {
     return (
         <div>
-            
+            <Navbar style={{backgroundColor: '#223843', width: '100%', height: '30px', bottom: '0'}}>
+            <ul style={{display: 'flex', flexDirection: 'row', alignContent: 'space-around', listStyleType: 'none'}}>
+            <li>
+              <IoMdHome color={'#F1F3F5'} size={20} />
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <IoMdAddCircle color={'#F1F3F5'} size={20} />
+              <Link to="/createlisting">Create Listing</Link>
+            </li>
+            <li>
+              <MdMessage color={'#F1F3F5'} size={20} />
+              <Link to="/messages">Messages</Link>
+            </li>
+            </ul>
+            </Navbar>
         </div>
     )
 }
