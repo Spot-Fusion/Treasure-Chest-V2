33adcc66e53a89a35527dfefdd2f7224fb3f599e
Thomas Garza <tbgarza2@gmail.com>
2020-04-11 17:12:10 -0500

    added title for header

--- src/pages/CreateListingPage.js
+++ src/pages/CreateListingPage.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import {useLocation} from 'react-router-dom'
 
-function CreateListingPage() {
+function CreateListingPage({title}) {
+    
+    let location = useLocation();
+    title(location.pathname);
     return (
         <div>
             <h2>Create Listing</h2>
