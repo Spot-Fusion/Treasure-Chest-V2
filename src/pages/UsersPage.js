894a510724fe6199330cabec1942b2a2c7277fc3
chris <christopherleboeuf1@gmail.com>
2020-04-14 16:17:55 -0500

    Basic navbar setup for user page

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,12 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Nav } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom'
 
-function UsersPage() {
-    return (
-        <div>
-            <h1>Following</h1>
-            <h1>Followers</h1>
-        </div>
-    )
+const UsersPage = () => {
+  const [active, setActive] = useState('Followers');
+  const location = useLocation();
+
+  const setStyle = (name) => active === name ? style.active : style.link;
+
+  const followNav = (name) => (
+    <Nav.Item as='li' onClick={() => setActive(name)}>
+      <Nav.Link style={setStyle(name)}>
+        {name}
+      </Nav.Link>
+    </Nav.Item>
+  )
+
+  return (
+    <div>
+      <div style={style.navBar}>
+        <Nav defaultActiveKey='/profile' as='ul' justify='true'>
+          {followNav('Following')}
+          {followNav('Followers')}
+        </Nav>
+      </div>
+    </div>
+  )
+}
+
+const style = {
+  navBar: {
+    position: 'fixed',
+    width: '100%',
+    top: '40px',
+    zIndex: '1000'
+  },
+
+  link: {
+    textDecoration: 'none',
+    color: 'black',
+    fontSize: '120%',
+    backgroundColor: 'white',
+    position: 'relative',
+    float: 'top',
+    borderBottom: '2px solid #D8D8D8'
+  },
+
+  active: {
+    textDecoration: 'none',
+    color: 'black',
+    fontSize: '120%',
+    backgroundColor: 'white',
+    position: 'relative',
+    float: 'top',
+    borderBottom: '2px solid #83AE9A'
+  }
 }
 
 export default UsersPage
