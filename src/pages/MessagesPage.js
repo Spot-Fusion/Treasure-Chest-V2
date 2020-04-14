06b1c595f7b17566ad4a9b42adf41e094071ba22
chris <christopherleboeuf1@gmail.com>
2020-04-13 19:06:21 -0500

    Changed messages to giv id, name, and icon of user

--- src/pages/MessagesPage.js
+++ src/pages/MessagesPage.js
@@ -1,33 +1,35 @@
 import * as React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav';
 
 const MessagesPage = () => {
   const [users, setUsers] = React.useState([]);
   const id = 13 || window.$user.id;
 
-  const getUsers = async () => {
-    let users = await axios.get(`http://localhost:8080/message/users/${id}`)
-    setUsers(users.data);
-  }
-
   React.useEffect(() => {
+    const getUsers = async () => {
+      let users = await axios.get(`http://localhost:8080/message/users/${id}`)
+      setUsers(users.data);
+    }
     getUsers();
     let intId = setInterval(() => { getUsers() }, 1000);
     return () => clearInterval(intId);
-  }, []);
+  }, [id]);
 
   return (
     <div>
       <h1>Messages</h1>
       <div>
         {!!users.length && users.map((user) => (
-          <Link to={{ pathname: "/chat", state: { id_recipient: user.id_sender === id ? user.id_recipient : user.id_sender} }} style={style.link} key={user.id_message}>
+          <Link to={{ pathname: "/chat", state: { id_recipient: user.id_sender === id ? user.id_recipient : user.id_sender, recipient_icon: user.icon, recipient_name: user.name } }} style={style.link} key={user.id_message}>
             <div>
-              <img src={user.icon} alt='' style={style.icon} />
-              <div style={style.name}>{user.name}</div>
-              <div style={style.date}>{user.last_sent_at.replace('T', ' ').replace('Z', '').split('.')[0]}</div>
-              <div style={style.message}>{user.text}</div>
+              <div>
+                <img src={user.icon} alt='' style={style.icon} />
+                <div style={style.name}>{user.name}</div>
+                <div style={style.date}>{user.last_sent_at.replace('T', ' ').replace('Z', '').split('.')[0]}</div>
+                  <div style={style.message}>{user.text}</div>
+              </div>
               <hr />
             </div>
           </Link>
