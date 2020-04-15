90f451cea92956fab0258e5c80cf59be4a94e929
chris <christopherleboeuf1@gmail.com>
2020-04-15 14:18:22 -0500

    Users page completed

--- src/pages/UsersPage.js
+++ src/pages/UsersPage.js
@@ -1,33 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { Nav, Button } from 'react-bootstrap';
-import { useLocation } from 'react-router-dom';
+import { Nav, Button, Modal } from 'react-bootstrap';
+import { useLocation, Link } from 'react-router-dom';
 import axios from 'axios';
 
 const UsersPage = () => {
-  const [active, setActive] = useState('Followers');
+  const [active, setActive] = useState('Following');
   const [users, setUsers] = useState([]);
-  // const location = useLocation();
-  const id = 13;
+  const [show, setShow] = useState(false);
+  const [username, setUsername] = useState('username');
+  const [clickedId, setId] = useState(0);
+  const location = useLocation();
+  const id = !location.state ? window.$user.id : location.state.id;
 
   const getFollowers = async () => {
-    let followers = await axios.get(`http://localhost:8080/follow/followed_by/${id}`);
+    const followers = await axios.get(`http://localhost:8081/follow/followed_by/${id}`);
     setUsers(followers.data);
-    console.log('hit 1', users);
   }
 
   const getFollowing = async () => {
-    let following = await axios.get(`http://localhost:8080/follow/following/${id}`);
+    const following = await axios.get(`http://localhost:8081/follow/following/${id}`);
     setUsers(following.data);
-    console.log('hit 2', users);
+  }
+
+  const unFollow = async () => {
+    await axios.delete(`http://localhost:8081/follow/${id}/${clickedId}`);
+    await getFollowing();
+    setShow(false);
   }
 
   const getUsers = (name) => {
-    setActive(name)
-    if(name === 'Followers'){
+    setActive(name);
+    setId(0);
+    if (name === 'Followers') {
       getFollowers();
     } else {
       getFollowing();
     }
+  }
+
+  const showUserModel = (name, userId) => {
+    setUsername(name);
+    setId(userId);
+    setShow(true);
   }
 
   const setStyle = (name) => active === name ? style.active : style.link;
@@ -40,6 +54,26 @@ const UsersPage = () => {
     </Nav.Item>
   )
 
+  const modal = (name) => {
+    return (
+      <div>
+        <Modal
+          className="modal"
+          show={show}
+          style={style.modal}
+          onHide={() => setShow(false)}
+          dialogClassName="modal-90w"
+          aria-labelledby="example-custom-modal-styling-title"
+        >
+          <Modal.Body>
+            <div style={style.unfollow} onClick={() => unFollow()}>Unfollow @{name}?</div>
+            <div onClick={() => setShow(false)}>Cancel</div>
+          </Modal.Body>
+        </Modal>
+      </div>
+    )
+  }
+
   useEffect(() => {
     getUsers(active);
   }, []);
@@ -51,17 +85,22 @@ const UsersPage = () => {
           {followNav('Following')}
           {followNav('Followers')}
         </Nav>
+
       </div>
       <div style={style.users}>
         {!!users.length && users.map((user) => (
-          <div key={user.id}>
-            <div>
-              <img src={user.icon} alt='' style={style.icon} />
-              <span>{user.name}</span>
-              {active === 'Following' && <Button style={style.button} variant="link">...</Button>}
+          <div style={style.user} key={user.id}>
+            <Link to={{ pathname: "/profile", state: { id: user.id } }} style={style.link}>
+              <div>
+                <img src={user.icon} alt='' style={style.icon} />
+                <div style={style.name}>{user.name}</div>
               </div>
+            </Link>
+            {active === 'Following' && window.$user === id && <Button style={style.button} onClick={() => showUserModel(user.name, user.id)} variant="link">...</Button>}
+            <br />
           </div>
         ))}
+        {window.$user === id && modal(username)}
       </div>
     </div>
   )
@@ -69,17 +108,48 @@ const UsersPage = () => {
 
 const style = {
 
-  button: {
-    textDecoration: 'none',
-    color: 'black',
+  modal: {
+    position: 'fixed',
+    marginTop: '120%',
+  },
+
+  unfollow: {
+    color: '#EA4335'
+  },
+
+  users: {
+    position: 'relative',
+    padding: '100px 0px 50px',
+  },
+
+  user: {
+    padding: '15px 0px 15px',
   },
 
   icon: {
-    width: '30px',
-    height: '30px',
+    width: '40px',
+    height: '40px',
     marginRight: '8px',
     objectFit: 'cover',
     borderRadius: '50%',
+    position: 'relative',
+    float: 'left',
+    left: '20px'
+  },
+
+  name: {
+    position: 'relative',
+    textAlign: 'left',
+    float: 'left',
+    left: '30px',
+    top: '5px'
+  },
+
+  button: {
+    textDecoration: 'none',
+    color: 'black',
+    position: 'relative',
+    float: 'right'
   },
 
   navBar: {
@@ -92,27 +162,21 @@ const style = {
   link: {
     textDecoration: 'none',
     color: 'black',
-    fontSize: '120%',
+    fontSize: '110%',
     backgroundColor: 'white',
     position: 'relative',
-    float: 'top',
     borderBottom: '2px solid #D8D8D8'
   },
 
   active: {
     textDecoration: 'none',
     color: 'black',
-    fontSize: '120%',
+    fontSize: '110%',
     backgroundColor: 'white',
     position: 'relative',
-    float: 'top',
     borderBottom: '2px solid #83AE9A'
   },
 
-  users: {
-    position: 'relative',
-    padding: '100px 0px 50px',
-  }
 }
 
 export default UsersPage
